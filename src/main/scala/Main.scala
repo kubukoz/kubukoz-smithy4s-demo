eac51aa5b3a966e1f5abe3614ca7b71aeb1ca040
Jakub Kozłowski <kubukoz@gmail.com>
2022-06-16 03:50:30 +0200

    use visitor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,23 +18,26 @@ import cats.implicits.given
 import cats.effect.IOApp
 import doobie.util.Read
 import doobie.util.Get
+import smithy4s.schema.SchemaVisitor
+import smithy4s.ShapeId
+import smithy4s.schema.Schema
+import smithy4s.schema.EnumValue
+import smithy4s.Lazy
+import smithy4s.schema.Primitive
+import smithy4s.schema.Alt
+import smithy4s.schema.Alt.WithValue
+import java.util.UUID
 object Main extends IOApp.Simple {
 
-  implicit val writeUser: Write[User] = User.schema
-    .compile(WriteSchematic)
-    .asInstanceOf[WriteOrPut.AWrite[User]]
-    .write
+  implicit val writeUser: Write[User] = User.schema.compile(ToWriteVisitor)
 
-  implicit val readUser: Read[User] = User.schema
-    .compile(ReadSchematic)
-    .asInstanceOf[ReadOrGet.ARead[User]]
-    .read
+  implicit val readUser: Read[User] = User.schema.compile(ToReadVisitor)
 
   val user = User(
-    UserId("example"),
+    UserId(UUID.randomUUID().toString),
     Username("u"),
     age = Some(42),
-    lastUpdatedAt = Some(Timestamp.fromEpochSecond(0L))
+    lastUpdatedAt = Some(Timestamp.nowUTC())
   )
 
   val xa = transactor.Transactor.fromDriverManager[IO](
@@ -49,123 +52,73 @@ object Main extends IOApp.Simple {
        users(id text primary key, name text not null, age int, lastUpdatedAt timestamp)""".update.run *>
       sql"""insert into users values($user)""".update.run *>
       sql"""select * from users""".query[User].nel
-  }.transact(xa).flatMap(IO.println(_))
+  }.transact(xa).flatMap(_.traverse_(IO.println(_)))
 }
 
-enum WriteOrPut[A] {
-  case AWrite(write: Write[A])
-  case APut(put: Put[A])
+object ToPutVisitor extends smithy4s.PolyFunction[Schema, Put] {
+  def apply[A](fa: Schema[A]): Put[A] = fa match {
+    case Schema.PrimitiveSchema(shapeId, hints, tag) =>
+      tag match {
+        case Primitive.PInt    => Put[Int]
+        case Primitive.PString => Put[String]
+        case Primitive.PTimestamp =>
+          Put[Instant].contramap[Timestamp](_.toInstant)
+      }
 
-  def contramap[B](f: B => A): WriteOrPut[B] = this match {
-    case AWrite(w) => AWrite(w.contramap(f))
-    case APut(p)   => APut(p.contramap(f))
+    case Schema.BijectionSchema(schema, f, g) =>
+      schema.compile(this).contramap(g)
   }
 }
 
-enum ReadOrGet[A] {
-  case ARead(read: Read[A])
-  case AGet(get: Get[A])
+object ToWriteVisitor extends smithy4s.PolyFunction[Schema, Write] {
+  def apply[S](fa: Schema[S]): Write[S] = fa match {
+    case Schema.StructSchema(shapeId, hints, fields, make) => {
+      val writes = fields.map { case (f: Field[Schema, S, a]) =>
+        f.mapK(ToPutVisitor)
+          .foldK(new Field.FolderK[Put, S, Write] {
+            def onOptional[A](
+                label: String,
+                instance: Put[A],
+                get: S => Option[A]
+            ): Write[Option[A]] = Write.fromPutOption(instance)
 
-  def map[B](f: A => B): ReadOrGet[B] = this match {
-    case ARead(r) => ARead(r.map(f))
-    case AGet(g)  => AGet(g.map(f))
-  }
-}
-
-import WriteOrPut._
-
-object WriteSchematic extends StubSchematic[WriteOrPut] {
-  def default[A]: WriteOrPut[A] = ???
-
-  override def string: WriteOrPut[String] = APut(Put[String])
-  override def bijection[A, B](
-      f: WriteOrPut[A],
-      to: A => B,
-      from: B => A
-  ): WriteOrPut[B] = f.contramap(from)
-
-  override def surjection[A, B](
-      f: WriteOrPut[A],
-      refinement: Refinement[A, B],
-      from: B => A
-  ): WriteOrPut[B] = f.contramap(from)
-
-  override def int: WriteOrPut[Int] = APut(Put[Int])
-
-  override def timestamp: WriteOrPut[Timestamp] =
-    APut(Put[Instant]).contramap(_.toInstant)
-
-  override def withHints[A](fa: WriteOrPut[A], hints: Hints): WriteOrPut[A] = fa
-
-  override def struct[S](fields: Vector[Field[WriteOrPut, S, _]])(
-      const: Vector[Any] => S
-  ): WriteOrPut[S] = WriteOrPut.AWrite {
-    val writes = fields.map { case (f: Field[WriteOrPut, S, a]) =>
-      val fieldPut: Field[Put, S, a] =
-        f.mapK(new (smithy4s.PolyFunction[WriteOrPut, Put]) {
-          def apply[A](wop: WriteOrPut[A]): Put[A] = wop match {
-            case APut(p) => p
-            case _       => sys.error("illegal")
-          }
-        })
-
-      fieldPut
-        .foldK(new Field.FolderK[Put, S, Write] {
-          def onOptional[A](
-              label: String,
-              instance: Put[A],
-              get: S => Option[A]
-          ): Write[Option[A]] = Write.fromPutOption(instance)
-
-          def onRequired[A](
-              label: String,
-              instance: Put[A],
-              get: S => A
-          ): Write[A] = Write.fromPut(instance)
-        })
-        .contramap(fieldPut.get)
-
-    }
-
-    writes.foldLeft(Write.unitComposite.contramap[S](_ => ()))(
-      (_, _).contramapN(b => (b, b))
-    )
-  }
-}
-
-import ReadOrGet._
-
-object ReadSchematic extends StubSchematic[ReadOrGet] {
-  def default[A]: ReadOrGet[A] = ???
-
-  override def string: ReadOrGet[String] = AGet(Get.apply)
-  override def int: ReadOrGet[Int] = AGet(Get.apply)
-  override def timestamp: ReadOrGet[Timestamp] = AGet(
-    Get[Instant].map(Timestamp.fromInstant(_))
-  )
-
-  override def withHints[A](fa: ReadOrGet[A], hints: Hints): ReadOrGet[A] = fa
-
-  override def bijection[A, B](
-      f: ReadOrGet[A],
-      to: A => B,
-      from: B => A
-  ): ReadOrGet[B] = f.map(to)
-
-  override def struct[S](fields: Vector[Field[ReadOrGet, S, ?]])(
-      const: Vector[Any] => S
-  ): ReadOrGet[S] = ARead {
-    fields
-      .map { case (f: Field[ReadOrGet, S, a]) =>
-        val fieldGet: Field[Get, S, a] =
-          f.mapK(new (smithy4s.PolyFunction[ReadOrGet, Get]) {
-            def apply[A](wop: ReadOrGet[A]): Get[A] = wop match {
-              case AGet(p) => p
-              case _       => sys.error("illegal")
-            }
+            def onRequired[A](
+                label: String,
+                instance: Put[A],
+                get: S => A
+            ): Write[A] = Write.fromPut(instance)
           })
+          .contramap(f.get)
 
-        fieldGet
+      }
+
+      writes.foldLeft(Write.unitComposite.contramap[S](_ => ()))(
+        (_, _).contramapN(b => (b, b))
+      )
+    }
+  }
+}
+
+object ToGetVisitor extends smithy4s.PolyFunction[Schema, Get] {
+  def apply[A](fa: Schema[A]): Get[A] = fa match {
+    case Schema.PrimitiveSchema(shapeId, hints, tag) =>
+      tag match {
+        case Primitive.PInt    => Get[Int]
+        case Primitive.PString => Get[String]
+        case Primitive.PTimestamp =>
+          Get[Instant].map(Timestamp.fromInstant(_))
+      }
+
+    case Schema.BijectionSchema(schema, f, g) =>
+      schema.compile(this).map(f)
+  }
+}
+
+object ToReadVisitor extends smithy4s.PolyFunction[Schema, Read] {
+  def apply[S](fa: Schema[S]): Read[S] = fa match {
+    case Schema.StructSchema(shapeId, hints, fields, make) => {
+      val reads = fields.map { case (f: Field[Schema, S, a]) =>
+        f.mapK(ToGetVisitor)
           .foldK(new Field.FolderK[Get, S, Read] {
             def onOptional[A](
                 label: String,
@@ -179,8 +132,12 @@ object ReadSchematic extends StubSchematic[ReadOrGet] {
                 get: S => A
             ): Read[A] = Read.fromGet(instance)
           })
+
       }
-      .traverse(a => a.widen[Any])
-      .map(const)
+
+      reads
+        .traverse(a => a.widen[Any])
+        .map(make)
+    }
   }
 }
